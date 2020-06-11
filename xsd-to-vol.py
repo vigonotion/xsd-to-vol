c8360295cbb39e315115c902daa658f3d4a138e1
Tom Schneider <tom.schneider-github@sutomaji.net>
2020-06-12 01:16:59 +0200

    Working generation

--- xsd-to-vol.py
+++ xsd-to-vol.py
@@ -10,7 +10,7 @@ def xsd_type_to_type(xsd_type):
         p_type = {
             "string": "str",
             "int": "int",
-            "long": "long",
+            "long": "int",
             "float": "float",
             "double": "float",
             "boolean": "bool",
@@ -33,9 +33,19 @@ with open('schema.xsd', 'r') as schema_file:
 #print(json.dumps(schema, indent=2))
 
 """
+Header.
+"""
+print("from voluptuous import In, Schema, Url")
+print()
+
+
+"""
 Simple types are like enums or like voluptuous In().
 """
-"""
+
+def simple_schema(values):
+    return f"In([{', '.join(vals)}])"
+
 for simple_type in schema["xsd:schema"]["xsd:simpleType"]:
     name = simple_type["@name"]
     docs = simple_type["xsd:annotation"]["xsd:documentation"].replace("\t\t\t\t", "")
@@ -48,95 +58,86 @@ for simple_type in schema["xsd:schema"]["xsd:simpleType"]:
         vals = [f"\"{enum['@value']}\""]
 
     print(f"\"\"\"\n{name}\n\n{docs}\n\"\"\"")
-    print(f"{name} = In([{', '.join(vals)}])")
+    print(f"{name} = {simple_schema(vals)}")
 
     print("\n\n")
-"""
+
 
 """
 Complex types.
 """
-"""
+
+def complex_schema(complex_type):
+    S = []
+    sequence = None
+
+    if "xsd:sequence" in complex_type:
+        sequence = complex_type["xsd:sequence"]
+    elif "xsd:complexContent" in complex_type and "xsd:sequence" in complex_type["xsd:complexContent"]:
+        sequence = complex_type["xsd:complexContent"]["xsd:sequence"]
+    elif "xsd:complexContent" in complex_type \
+        and "xsd:extension" in complex_type["xsd:complexContent"] \
+        and "xsd:sequence" in complex_type["xsd:complexContent"]["xsd:extension"]:
+        sequence = complex_type["xsd:complexContent"]["xsd:extension"]["xsd:sequence"]
+    
+    if sequence:
+        for element in sequence["xsd:element"] if isinstance(sequence["xsd:element"], list) else [sequence["xsd:element"]]:
+            name = element["@name"]
+            doc = element["xsd:annotation"]["xsd:documentation"] if "xsd:annotation" in element else ""
+            e_type = xsd_type_to_type(element["@type"])
+
+            if "default" in element:
+                doc += f"\nDefault: {element['@default']}"
+
+            #docs += (f"{name}: {e_type} {doc}\n")
+
+            S.append(f"\"{name}\": {e_type}")
+    
+    #print(f"{docs}\"\"\"")
+
+    if "xsd:complexContent" in complex_type:
+        base = xsd_type_to_type(complex_type["xsd:complexContent"]["xsd:extension"]["@base"])
+        s = "{" + ", ".join(S) + "}"
+        return f"Schema.extend({base}, {s})"
+
+    return "Schema({" + ", ".join(S) + "})"
+
 for complex_type in schema["xsd:schema"]["xsd:complexType"]:
     type_name = complex_type["@name"]
     docs = complex_type.get("xsd:annotation", {}).get("xsd:documentation", "")
     if docs != "":
         docs += "\n\n"
 
-    print(f"\"\"\"\n{type_name}\n")
+    print(f"\"\"\"\n{type_name}\n\"\"\"")
 
-    S = []
-    if "xsd:sequence" in complex_type:
-        sequence = complex_type["xsd:sequence"]
-    elif "xsd:complexContent" in complex_type and "xsd:sequence" in complex_type["xsd:complexContent"]:
-        sequence = complex_type["xsd:complexContent"]["xsd:sequence"]
-    
-    if sequence:
-        for element in sequence["xsd:element"] if isinstance(sequence["xsd:element"], list) else [sequence["xsd:element"]]:
-            name = element["@name"]
-            doc = element["xsd:annotation"]["xsd:documentation"] if "xsd:annotation" in element else ""
-            e_type = xsd_type_to_type(element["@type"])
+    s = complex_schema(complex_type)
 
-            if "default" in element:
-                doc += f"\nDefault: {element['@default']}"
-
-            docs += (f"{name}: {e_type} {doc}\n")
-
-            S.append(f"\"{name}\": {e_type}")
-    
-    print(f"{docs}\"\"\"")
-
-    schema = "{" + ", ".join(S) + "}"
-
-    if "xsd:complexContent" in complex_type:
-        base = xsd_type_to_type(complex_type["xsd:complexContent"]["xsd:extension"]["@base"])
-        print(f"{type_name} = Schema.extend({base}, {schema})")
-    else:
-        print(f"{type_name} = Schema({schema})")
+    print(f"{type_name} = {s}")
 
     print("\n\n")
 
-"""
+
 
 """
 Elements.
 """
+
 for element_type in schema["xsd:schema"]["xsd:element"]:
     type_name = element_type["@name"]
     docs = element_type.get("xsd:annotation", {}).get("xsd:documentation", "")
     if docs != "":
         docs += "\n\n"
 
-    print(f"\"\"\"\n{type_name}\n")
+    print(f"\"\"\"\n{type_name}\n\"\"\"")
 
-    S = []
-    sequence = None
-    if "xsd:sequence" in element_type["xsd:complexType"]:
-        sequence = element_type["xsd:complexType"]["xsd:sequence"]
-    elif "xsd:complexContent" in element_type["xsd:complexType"] and "xsd:sequence" in element_type["xsd:complexType"]["xsd:complexContent"]:
-        sequence = element_type["xsd:complexType"]["xsd:complexContent"]["xsd:sequence"]
-    
-    if sequence:
-        for element in sequence["xsd:element"] if isinstance(sequence["xsd:element"], list) else [sequence["xsd:element"]]:
-            name = element["@name"]
-            doc = element["xsd:annotation"]["xsd:documentation"] if "xsd:annotation" in element else ""
-            e_type = xsd_type_to_type(element["@type"])
+    # more docs
 
-            if "default" in element:
-                doc += f"\nDefault: {element['@default']}"
+    #print(f"\"\"\"\n")
 
-            docs += (f"{name}: {e_type} {doc}\n")
-
-            S.append(f"\"{name}\": {e_type}")
-    
-    print(f"{docs}\"\"\"")
-
-    schema = "{" + ", ".join(S) + "}"
-
-    if "xsd:complexContent" in element_type:
-        base = xsd_type_to_type(element_type["xsd:complexContent"]["xsd:extension"]["@base"])
-        print(f"{type_name} = Schema.extend({base}, {schema})")
+    if "xsd:complexType" in element_type:
+        print(complex_schema(element_type["xsd:complexType"]))
     else:
-        print(f"{type_name} = Schema({schema})")
+        raise Exception("No complexType!")
+    
 
     print("\n\n")
